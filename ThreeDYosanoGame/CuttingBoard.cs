0b494210a982c334499f31a86b6d051884c3e963
Eulerd <euler.volcano.dai@gmail.com>
2015-12-24 13:23:43 +0900

    四角いまな板を三角で描写

--- ThreeDYosanoGame/CuttingBoard.cs
+++ ThreeDYosanoGame/CuttingBoard.cs
@@ -15,18 +15,13 @@ namespace _3DActionGame__
         public DX.VECTOR Point4 { get; set; }
         public uint Color { get; set; }
 
-        public CuttingBoard(int x1,int y1,int z1,int x2, int y2, int z2, int x3, int y3, int z3, int x4, int y4, int z4, uint color)
+        public void Draw(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3, int x4, int y4, int z4, uint color)
         {
             Point1 = DX.VGet(x1, y1, z1);
             Point2 = DX.VGet(x2, y2, z2);
             Point3 = DX.VGet(x3, y3, z3);
             Point4 = DX.VGet(x4, y4, z4);
             Color = color;
-        }
-
-
-        public void Draw()
-        {
             DX.DrawTriangle3D(Point1, Point2, Point4, Color, 1);
             DX.DrawTriangle3D(Point2, Point3, Point4, Color, 1);
         }
